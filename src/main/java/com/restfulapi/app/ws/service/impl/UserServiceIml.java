c88672b1d08a0d861d0426fa5f23177043f13c55
Azhar <azadbasis@gmail.com>
2019-03-24 21:22:53 +0600

    Sign up(set default value,check if user already exist,Generate user public ID)

--- src/main/java/com/restfulapi/app/ws/service/impl/UserServiceIml.java
+++ src/main/java/com/restfulapi/app/ws/service/impl/UserServiceIml.java
@@ -3,6 +3,7 @@ package com.restfulapi.app.ws.service.impl;
 import com.restfulapi.app.ws.UserRepository;
 import com.restfulapi.app.ws.io.entity.UserEntity;
 import com.restfulapi.app.ws.service.UserService;
+import com.restfulapi.app.ws.shared.Utils;
 import com.restfulapi.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,22 @@ public class UserServiceIml implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail())!=null)throw new RuntimeException("Record already exists");
 
         UserEntity userEntity=new UserEntity();
         BeanUtils.copyProperties(user,userEntity);
 
+        String publicUserId=utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
+
         userEntity.setEncryptedPassword("Test");
-        userEntity.setUserId("TestUserId");
+
         UserEntity storedUserDetails=userRepository.save(userEntity);
 
         UserDto returnValue=new UserDto();
